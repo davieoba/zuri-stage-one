da5630a0755ab8ad990ed7be26b4cfb983985e0b
davieoba <bodunrindavidbond@gmail.com>
2023-09-07 04:55:26 +0100

    feat: update

--- routes/info-route.js
+++ routes/info-route.js
@@ -1,33 +1,22 @@
 const router = require('express').Router()
 
+// http://example.com/api?slack_name=example_name&track=backend
+// http://localhost:5000/api/v1/info?slack_name=David Bodunrin&track=backend
 router.get('/', (req, res) => {
   const track = req.query.track
   const slack_name = req.query.slack_name
 
   const current_day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][new Date().getDay()]
 
-  console.log(new Date())
-
   res.status(200).json({
     slack_name,
     current_day,
     utc_time: new Date(),
     track,
+    github_file_url: 'https://github.com/davieoba/zuri-stage-one/blob/master/routes/info-route.js',
+    github_repo_url: 'https://github.com/davieoba/zuri-stage-one',
+    status_code: 200
   })
 })
+
 module.exports = router
-
-/**
- * 
- * {
-  "slack_name": "example_name",
-  "current_day": "Monday",
-  "utc_time": "2023-08-21T15:04:05Z",
-  "track": "backend",
-  "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-  "github_repo_url": "https://github.com/username/repo",
-  "status_code": 200
-}
-
- http://example.com/api?slack_name=example_name&track=backend
- */
