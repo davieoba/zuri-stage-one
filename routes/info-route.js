d4b866eea00aefcbd9067c8f914d4dd743f9a984
davieoba <bodunrindavidbond@gmail.com>
2023-09-08 01:07:36 +0100

    update url endpoint

--- routes/info-route.js
+++ routes/info-route.js
@@ -8,10 +8,24 @@ router.get('/', (req, res) => {
 
   const current_day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][new Date().getDay()]
 
+  const currentDate = new Date()
+
+  // Get the components of the date and time
+  const year = currentDate.getUTCFullYear()
+  const month = String(currentDate.getUTCMonth() + 1).padStart(2, '0') // Month is zero-based, so add 1
+  const day = String(currentDate.getUTCDate()).padStart(2, '0')
+  const hours = String(currentDate.getUTCHours()).padStart(2, '0')
+  const minutes = String(currentDate.getUTCMinutes()).padStart(2, '0')
+  const seconds = String(currentDate.getUTCSeconds()).padStart(2, '0')
+
+  // Create the formatted date string
+  const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+
+
   res.status(200).json({
     slack_name,
     current_day,
-    utc_time: new Date(),
+    utc_time: formattedDate,
     track,
     github_file_url: 'https://github.com/davieoba/zuri-stage-one/blob/master/routes/info-route.js',
     github_repo_url: 'https://github.com/davieoba/zuri-stage-one',
